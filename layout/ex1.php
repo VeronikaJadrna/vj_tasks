c342cb94036162bea6f869901615a05670e8a622
VeronikaJadrna <jadrna.veronika@gmail.com>
2023-02-01 11:16:59 +0200

    excercise 1 done

--- layout/ex1.php
+++ layout/ex1.php
@@ -2,7 +2,7 @@
 <?php 
 $title = "Excercise 1";
 include "header.php"?>
-    <h1>3.1. Write a simple PHP script to print your information (Name and your groupID)</h1>
+    <h2>3.1. Write a simple PHP script to print your information (Name and your groupID)</h2>
 
     
 <?php
@@ -10,19 +10,48 @@ echo "Veronika Jadrna<br>";
 echo "BBCAP22";
 ?>
 
-<h1>3.2 Write PHP code to display the folowing message</h1>
+<h2>3.2 Write PHP code to display the folowing message</h2>
 
 <?php 
 echo "Hello World! My name is \"Veronika\"";
 ?>
 
-<h1>3.3 Write the PHP in to display the current date.</h1>
+<h2>3.3 Write the PHP in to display the current date.</h2>
 
-<?php 
-echo date('d.M.y');
-?>
+<?php
+echo date('d.M.y');?>
+
+
+<h2>3.4 Variable in php</h2>
 
 <?php include "footer.php"?>
+<?php
+$title1="PHP is interesting";
+echo "<h1>" . $title1 . "</h1>";
+?>
 
+<h2>3.5.Grades</h2>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
 
-
+echo "
+<table>
+<tr>
+<th> S.N.</th>
+<th>Name</th>
+<th>Grade</th>
+</tr>
+<tr>
+<td> 1 </td><td> Pekka </td><td> $g1 </td>
+</tr>
+<tr>
+<td> 2 </td><td> Johanna </td><td> $g2 </td>
+</tr>
+<tr>
+<td> 3 </td><td> John </td><td> $g3 </td>
+</tr>
+</table>
+"
+?>
