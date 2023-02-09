190e8be26c2e56d3e10e09c400978cad363da716
VeronikaJadrna <jadrna.veronika@gmail.com>
2023-02-09 21:39:02 +0200

    change

--- layout/footer.php
+++ layout/footer.php
@@ -7,10 +7,12 @@
     </div>
     <div class="footer-copyright text-center py-3"><p>&copy; Veronika Jadrna 2023
     <br><?php
+
     echo $_SERVER['PHP_SELF'];
+
     $fname = basename($_SERVER['PHP_SELF']);
     $last_modified = filemtime($fname);
-    echo "Last modified on: " . date('l js \o\f F Y, h:i:s A', $last_modified);
+    echo "Last modified on: " . date('l jS \o\f F Y, h:i:s A', $last_modified);
     ?>
    </footer>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
